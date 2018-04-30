1f6e807678fc21c3f3b4cc148d7f69d46d1b945a
David Fisher <fisherds@rose-hulman.edu>
2018-04-30 12:10:51 -0400

    Added the code to use the camera and Photo Library

--- Favorite Photos/FavoritePhotoViewController.swift
+++ Favorite Photos/FavoritePhotoViewController.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class FavoritePhotoViewController: UIViewController {
-
   @IBOutlet weak var imageView: UIImageView!
-  
   @IBOutlet weak var progressView: UIProgressView!
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
 
   @IBAction func takePhoto(_ sender: Any) {
-    
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      imagePicker.sourceType = .camera
+    } else {
+      imagePicker.sourceType = .photoLibrary
+    }
+    present(imagePicker, animated: true)
   }
 }
+
+// MARK: UIImagePicker controller delegate methods
+
+extension FavoritePhotoViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true)
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+
+      // TODO: Upload the data to Storage, display AFTER the Storage save is done
+      self.imageView.image = image // Cheat TODO: Delete this line!
+    }
+    picker.dismiss(animated: true)
+  }
+}
+
+
+
+
+
+
+
+
+
+
